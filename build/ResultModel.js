9fc73e0a436bd9d053f15a25c5a92118beeadea2
jns-br <jonasdone@gmail.com>
2020-01-30 13:33:21 +0100

    update result model to categorize answers by alphas

--- build/ResultModel.js
+++ build/ResultModel.js
@@ -1,7 +1,36 @@
 const mongoose = require('mongoose');
 
 const resultSchema = mongoose.Schema({
-  result: Object
+  alpha77: {
+    lifeOfParty: {type: Number, required: true},
+    talkAtParties: {type: Number, required: true},
+    dontTalk: {type: Number, required:true},
+    keepInBackground: {type: Number, required: true}
+  },
+  alpha70: {
+    sympathize: {type: Number, required: true},
+    othersEmos: {type: Number, required: true},
+    interestInOthers: {type: Number, required: true},
+    othersProblems: {type: Number, required: true}
+  },
+  alpha69: {
+    chores: {type: Number, required: true},
+    order: {type: Number, required: true},
+    forgetThings: {type: Number, required: true},
+    mess: {type: Number, required: true}
+  },
+  alpha68: {
+    moodSwings: {type: Number, required: true},
+    upset: {type: Number, required: true},
+    relaxed: {type: Number, required: true},
+    blue: {type: Number, required: true}
+  },
+  alpha65: {
+    vividImagination: {type: Number, required: true},
+    abstractUnderstanding: {type: Number, required: true},
+    abstractInterest: {type: Number, required: true},
+    noImagination: {type: Number, required: true}
+  }
 });
 
 module.exports = mongoose.model('Result', resultSchema);
