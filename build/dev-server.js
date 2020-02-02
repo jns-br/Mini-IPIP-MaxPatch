41911015c40f6236d4861f95b71f3b047032d17f
jns-br <jonasdone@gmail.com>
2020-02-02 13:14:46 +0100

    remove csp config

--- build/dev-server.js
+++ build/dev-server.js
@@ -69,13 +69,6 @@ app.use(hotMiddleware);
 
 app.use(bodyparser.json());
 
-const csp = require('express-csp-header');
-app.use(csp({
-  policies: {
-    'default-src': [csp.NONE],
-    'img-src': [csp.SELF],
-  }
-}));
 
 DBService.init();
 
