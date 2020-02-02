d935b46fded75f0ae2ecb51fa323807f60067269
jns-br <jonasdone@gmail.com>
2020-02-02 09:11:33 +0100

    refactor dev server and add request

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,88 +1,98 @@
-require('./check-versions')()
+require('./check-versions')();
 
-const config = require('../config')
+const config = require('../config');
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 }
 
-const opn = require('opn')
-const bodyparser = require('body-parser')
-const path = require('path')
-const express = require('express')
-const webpack = require('webpack')
-const proxyMiddleware = require('http-proxy-middleware')
+const request = require('request-promise');
+const opn = require('opn');
+const bodyparser = require('body-parser');
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const proxyMiddleware = require('http-proxy-middleware');
 const webpackConfig = process.env.NODE_ENV === 'testing'
   ? require('./webpack.prod.conf')
-  : require('./webpack.dev.conf')
+  : require('./webpack.dev.conf');
 const DBService = require('./DBService');
 const ResultRepo = require('./ResultRepo');
 
+const maxPort = config.dev.env.MAX_SERVER;
 
 // default port where dev server listens for incoming traffic
-const port = process.env.PORT || config.dev.port
+const port = process.env.PORT || config.dev.port;
 // automatically open browser, if not set will be false
-const autoOpenBrowser = !!config.dev.autoOpenBrowser
+const autoOpenBrowser = !!config.dev.autoOpenBrowser;
 // Define HTTP proxies to your custom API backend
 // https://github.com/chimurai/http-proxy-middleware
-const proxyTable = config.dev.proxyTable
+const proxyTable = config.dev.proxyTable;
 
-const app = express()
-const compiler = webpack(webpackConfig)
+const app = express();
+const compiler = webpack(webpackConfig);
 
 const devMiddleware = require('webpack-dev-middleware')(compiler, {
   publicPath: webpackConfig.output.publicPath,
   quiet: true
-})
+});
 
 const hotMiddleware = require('webpack-hot-middleware')(compiler, {
   log: () => {}
-})
+});
+
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
+    hotMiddleware.publish({ action: 'reload' });
     cb()
   })
-})
+});
 
 // proxy api requests
 Object.keys(proxyTable).forEach(function (context) {
-  var options = proxyTable[context]
+  const options = proxyTable[context];
   if (typeof options === 'string') {
     options = { target: options }
   }
   app.use(proxyMiddleware(options.filter || context, options))
-})
+});
 
 // handle fallback for HTML5 history API
-app.use(require('connect-history-api-fallback')())
+app.use(require('connect-history-api-fallback')());
 
 // serve webpack bundle output
-app.use(devMiddleware)
+app.use(devMiddleware);
 
 // enable hot-reload and state-preserving
 // compilation error display
-app.use(hotMiddleware)
+app.use(hotMiddleware);
 
-app.use(bodyparser.json())
+app.use(bodyparser.json());
 
 DBService.init();
 
 // serve pure static assets
-const staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
-app.use(staticPath, express.static('./static'))
+const staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory);
+app.use(staticPath, express.static('./static'));
 
-const uri = 'http://localhost:' + port
+const uri = 'http://localhost:' + port;
 
-devMiddleware.waitUntilValid(function () {
+devMiddleware.waitUntilValid(() => {
   console.log('> Listening at ' + uri + '\n')
-})
+});
 
 app.post('/', async (req, res) => {
   try {
     const result = req.body;
 
-    await ResultRepo.saveResult(result);
+    const entry = await ResultRepo.saveResult(result);
+
+    await request({
+      method: 'POST',
+      uri: maxPort,
+      body: entry,
+      json: true
+    });
 
     res.json()
   } catch (err) {
@@ -90,10 +100,11 @@ app.post('/', async (req, res) => {
     res.status(500).json({message: err.message});
   }
 
-})
+});
+
 module.exports = app.listen(port, function (err) {
   if (err) {
-    console.log(err)
+    console.log(err);
     return
   }
 
