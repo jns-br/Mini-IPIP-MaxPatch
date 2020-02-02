1dca115958835076fba3681fd3f4027fab1a1014
jns-br <jonasdone@gmail.com>
2020-02-02 08:54:13 +0100

    update ResultModel

--- build/ResultModel.js
+++ build/ResultModel.js
@@ -1,31 +1,31 @@
 const mongoose = require('mongoose');
 
 const resultSchema = mongoose.Schema({
-  alpha77: {
+  extraversion: {
     lifeOfParty: {type: Number, required: true},
     talkAtParties: {type: Number, required: true},
     dontTalk: {type: Number, required:true},
     keepInBackground: {type: Number, required: true}
   },
-  alpha70: {
+  agreeableness: {
     sympathize: {type: Number, required: true},
     othersEmos: {type: Number, required: true},
     interestInOthers: {type: Number, required: true},
     othersProblems: {type: Number, required: true}
   },
-  alpha69: {
+  conscientiousness: {
     chores: {type: Number, required: true},
     order: {type: Number, required: true},
     forgetThings: {type: Number, required: true},
     mess: {type: Number, required: true}
   },
-  alpha68: {
+  neuroticism: {
     moodSwings: {type: Number, required: true},
     upset: {type: Number, required: true},
     relaxed: {type: Number, required: true},
     blue: {type: Number, required: true}
   },
-  alpha65: {
+  imagination: {
     vividImagination: {type: Number, required: true},
     abstractUnderstanding: {type: Number, required: true},
     abstractInterest: {type: Number, required: true},
